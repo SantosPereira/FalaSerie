f32d6880195662a558a97fd89633fea07ed5c524
SantosPereira <pedrohenriquelemam@gmail.com>
2022-02-06 17:53:45 -0300

    Segunda tela

---
+++ src/main/java/com/longa/falaserie/Listagem.java
@@ -0,0 +1,14 @@
+package com.longa.falaserie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listagem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem);
+    }
+}
