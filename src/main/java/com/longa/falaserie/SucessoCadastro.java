9cd5ef9d69c1d7423380540c30ed6f2895d5a40b
SantosPereira <pedrohenriquelemam@gmail.com>
2022-02-07 09:01:57 -0300

    UI implementada

---
+++ src/main/java/com/longa/falaserie/SucessoCadastro.java
@@ -0,0 +1,14 @@
+package com.longa.falaserie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SucessoCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucesso_cadastro);
+    }
+}
